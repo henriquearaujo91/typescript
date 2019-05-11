77399fd52fbdb5c613083246a3d3a235e8eb92ec
Henrique Araujo <henrique_araujo91@outlook.com>
2019-05-11 15:25:52 -0300

    modificando conteudo da tag p

--- ts/HelloWorld.ts
+++ ts/HelloWorld.ts
@@ -1,8 +1,18 @@
 class Startup {
-    public static main(): number {
-        console.log('Hello World');
-        return 0;
+    public static main(texto: string): number {
+        console.log(texto);
+        return texto.length;
     }
 }
 
-Startup.main();
+var hello: string = "Hello World";
+var quantidadeCaracteres: number = Startup.main(hello);
+
+try {
+    //modificando conteudo da tag 'p'
+    var p = document.createElement('p');
+    p.textContent = hello.concat(" possui ", quantidadeCaracteres.toString(), " caracteres");
+    document.body.appendChild(p);
+} catch (error) {
+    console.log("Não foi executado via browser!!!");
+}
