4ec8a6be293ae2473bfc35149e8830ade04c9c1b
Henrique Araujo <henrique_araujo91@outlook.com>
2019-05-11 15:26:40 -0300

    arquivos .js e .js.map criados quando o arquivo .ts foi compilado

--- ts/HelloWorld.js
+++ ts/HelloWorld.js
@@ -1,11 +1,21 @@
 var Startup = /** @class */ (function () {
     function Startup() {
     }
-    Startup.main = function () {
-        console.log('Hello World');
-        return 0;
+    Startup.main = function (texto) {
+        console.log(texto);
+        return texto.length;
     };
     return Startup;
 }());
-Startup.main();
+var hello = "Hello World";
+var quantidadeCaracteres = Startup.main(hello);
+try {
+    //modificando conteudo da tag 'p'
+    var p = document.createElement('p');
+    p.textContent = hello.concat(" possui ", quantidadeCaracteres.toString(), " caracteres");
+    document.body.appendChild(p);
+}
+catch (error) {
+    console.log("Não foi executado via browser!!!");
+}
 //# sourceMappingURL=HelloWorld.js.map
