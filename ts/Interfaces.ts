6effa2535c584f21dcdc181af58a9e53a46b05fb
Henrique Araujo <henrique_araujo91@outlook.com>
2019-05-12 11:20:14 -0300

    criando interface carro

--- ts/Interfaces.ts
+++ ts/Interfaces.ts
@@ -19,3 +19,16 @@ printName({ nome: "Fernando" } as Empregado);
 
 let empregado2: Empregado = { nome: "João", idade: 27, salario: 2500 };
 console.log(empregado2);
+
+interface ICarro {
+    modelo: string;
+    anoFabricacao: number;
+    licenciado?: boolean;
+}
+function detalhesVeicuo(carro: ICarro): void {
+    console.log(`Modelo do carro: ${carro.modelo}
+Ano de Fabricação: ${carro.anoFabricacao}`.concat(
+        carro.licenciado ? ("\nVeiculo licenciado") : "\nVeiculo não licenciado"))
+}
+detalhesVeicuo({ modelo: "Ferrari", anoFabricacao: 2019 });
+detalhesVeicuo({ modelo: "BMW", anoFabricacao: 2018, licenciado: true });
--- ts/interfaces.js
+++ ts/interfaces.js
@@ -8,4 +8,9 @@ printName(empregado);
 printName({ nome: "Fernando" });
 var empregado2 = { nome: "João", idade: 27, salario: 2500 };
 console.log(empregado2);
+function detalhesVeicuo(carro) {
+    console.log(("Modelo do carro: " + carro.modelo + "\nAno de Fabrica\u00E7\u00E3o: " + carro.anoFabricacao).concat(carro.licenciado ? ("\nVeiculo licenciado") : "\nVeiculo não licenciado"));
+}
+detalhesVeicuo({ modelo: "Ferrari", anoFabricacao: 2019 });
+detalhesVeicuo({ modelo: "BMW", anoFabricacao: 2018, licenciado: true });
 //# sourceMappingURL=interfaces.js.map
