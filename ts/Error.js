4ef9c5de8ef592c1112930e06ba818cddf1ea041
Henrique Araujo <henrique_araujo91@outlook.com>
2019-05-12 10:41:04 -0300

    forEach+restParameters+ArrowFunction

--- ts/Error.js
+++ ts/Error.js
@@ -4,6 +4,17 @@ var calculaIOF = function (numero1, numero2) {
     }
     return numero1 / numero2 * 10;
 };
+var calculaSoma = function (nuemro1, numero2) {
+    var listaNumeros = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        listaNumeros[_i - 2] = arguments[_i];
+    }
+    var retorno = 0;
+    listaNumeros.forEach(function (numero) {
+        retorno += numero;
+    });
+    return retorno + nuemro1 + numero2;
+};
 try {
     console.log(calculaIOF(2, 0));
 }
@@ -11,6 +22,7 @@ catch (error) {
     console.log(error);
 }
 finally {
+    console.log(calculaSoma(1, 2, 3, 4, 5, 6));
     console.log("Fim da opera\u00E7\u00E3o!");
 }
 //# sourceMappingURL=Error.js.map
--- ts/Error.ts
+++ ts/Error.ts
@@ -5,10 +5,19 @@ let calculaIOF = (numero1: number, numero2: number) => {
     return numero1 / numero2 * 10;
 }
 
+let calculaSoma = (nuemro1: number, numero2: number, ...listaNumeros: number[]) => {
+    let retorno: number = 0;
+    listaNumeros.forEach(numero => {
+        retorno += numero;
+    });
+    return retorno + nuemro1 + numero2;
+}
+
 try {
     console.log(calculaIOF(2, 0));
 } catch (error) {
     console.log(error);
-} finally{
+} finally {
+    console.log(calculaSoma(1,2,3,4,5,6));
     console.log(`Fim da operação!`);
 }
